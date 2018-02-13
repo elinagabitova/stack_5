c8bab9702c0f2ecc9929dc88a3f3f0b5708f642c
Elina Gabitova <elinagabitovaa@gmail.com>
2018-02-14 02:44:20 +0500

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,30 +1,27 @@
-#ifndef STACK_HPP 
-#define STACK_HPP
-
 #include <iostream>
-#include <stdexcept>
 #include <algorithm>
-#include <utility>
+#include <new>
+#include <thread>
+#include <mutex>
+#include <memory>
 
 template <typename T>
 class stack
 {
+private:
+	void swap(stack<T> &);
+
 public:
 	stack();
-	~stack();
-	stack(size_t size);
-	stack(stack<T> const &copy);
-	stack<T>& operator = (stack<T> const &other);
+	stack(stack<T> const &);
+	stack<T>& operator = (stack<T> const &);
 	size_t count() const;
-	size_t array_size() const;
-	void swap(stack<T>& other);
 	void push(T const &);
-	T pop();
-	std::ostream& print(std::ostream& os);
-			
-	
-	
+	auto pop() -> std::shared_ptr<T>;
+	bool empty() const;
+
 private:
+	mutable std::mutex mutex_;
 	T* array_;
 	size_t array_size_;
 	size_t count_;
@@ -35,112 +32,103 @@ stack<T>::stack() : count_(0), array_size_(0), array_(nullptr)
 {}
 
 template <typename T>
-stack<T>::stack(size_t size)
+size_t stack<T>::count() const
 {
-        count_ = size;
-        array_size_ = size;;
-        array_ = new T[size]();
+	std::lock_guard<std::mutex> lock(mutex_);
+	return count_;
 }
-	
 
 template <typename T>
-stack<T>::stack(stack<T> const &copy)
+stack<T>::stack(stack<T> const& copy)
 {
+	std::lock_guard<std::mutex> lock(copy.mutex_);
+	T* temp = new T[copy.array_size_];
 	array_size_ = copy.array_size_;
 	count_ = copy.count_;
-	array_ = new T[array_size_];
-
-	std::copy(copy.array_, copy.array_ + copy.array_size_, array_);
+	array_ = temp;
+	try
+	{
+		std::copy(copy.array_, copy.array_ + count_, array_);
+	}
+	catch (...)
+	{
+		delete[] array_;
+		throw;
+	}
 }
 
 template <typename T>
-stack<T>& stack<T>::operator=(stack<T> const &other)
+stack<T>& stack<T>::operator=(stack<T> const& other)
 {
 	if (this != &other)
 	{
 		stack<T> temp(other);
 		swap(temp);
 	}
+
 	return *this;
-}
-
-template <typename T>
-stack<T>::~stack()
-{
-	delete[] array_;
-}
-
-template <typename T>
-size_t stack<T>::count() const
-{
-	return count_;
-}
-
-template <typename T>
-size_t stack<T>::array_size() const
-{
-	return array_size_;
-}
-
-template <typename T>
-void stack<T>::push(T const &value)
-{
-	if (array_size_ == 0)
-	{
-		array_size_ = 1;
-		array_ = new T[array_size_];
-	}
-	else if (count_ + 1 >= array_size_)
-	{
-		array_size_ = array_size_ * 2;
-		T* temp = new T[array_size_];
-		std::copy(array_, array_ + count_, temp);
-
-		delete[] array_;
-
-		array_ = temp;
-	}
-	array_[count_++] = value;
-}
-
-template <typename T>
-T stack<T>::pop()
-{
-	if (count_ == 0)
-		throw std::logic_error("Stack is empty! Try again!\n");
-	else
-	{
-		T temp = array_[--count_];
-
-		return temp;
-	}
-}
-
-template <typename T>
-std::ostream& stack<T>::print(std::ostream& os)
-{
-	if (count_ == 0)
-		os << "Stack is empty! Try again!\n";
-	else
-		for (unsigned int i = 0; i < count_; i++)
-		{
-			os << array_[i] << "\t";
-		}
-	
-	return os;
-}
-
-template <typename T>
-std::ostream& operator<< (std::ostream& os, stack<T>& obj)
-{
-	return obj.print(os);
 }
 
 template <typename T>
 void stack<T>::swap(stack<T>& other)
 {
+	std::lock(mutex_, other.mutex_);
 	std::swap(array_, other.array_);
-    	std::swap(array_size_, other.array_size_);
+	std::swap(array_size_, other.array_size_);
 	std::swap(count_, other.count_);
+	mutex_.unlock();
+	other.mutex_.unlock();
 }
-#endif
+
+template <typename T>
+void stack<T>::push(T const& value)
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	if (array_size_ == count_)
+	{
+		size_t size = array_size_ == 0 ? 1 : array_size_ * 2;
+
+		T* temp = new T[size];
+		
+		try
+		{
+			std::copy(array_, array_ + count_, temp);	
+		}
+		
+		catch ( ... )
+		{
+			delete[] temp;
+			throw;
+		}
+
+		array_size_ = size;
+		delete[] array_;
+		array_ = temp;
+	}
+	
+	array_[count_] = value;
+	++count_;
+}
+
+template <typename T>
+auto stack<T>::pop() -> std::shared_ptr<T>
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	if (empty())
+	{
+		throw "Stack is empty! Try again!\n";
+	}
+	else
+	{
+		--count_;
+	}
+	auto top = std::make_shared<T>(array_[count_]);
+	return top;
+}
+
+template <typename T>
+bool stack<T>::empty() const
+{
+	std::lock_guard<std::mutex> lock(mutex_);
+	return (count_ == 0);	
+}
