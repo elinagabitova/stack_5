de2714d59d5bf46f0093ca4a53bc0f7c41702dbd
Elina Gabitova <elinagabitovaa@gmail.com>
2018-02-14 02:44:58 +0500

    Update stack.cpp

--- tests/stack.cpp
+++ tests/stack.cpp
@@ -1,6 +1,7 @@
 #include <catch.hpp>
 
 #include "stack.hpp"
+
 TEST_CASE("push", "") {
     stack<int> s1;
     s1.push(1);
@@ -11,20 +12,12 @@ TEST_CASE("push", "") {
     REQUIRE(s1.count() == 5);
 }
 
-TEST_CASE("pop", "") {
+TEST_CASE("pop") 
+{
     stack<int> s1;
-    s1.push(1);
-    s1.push(2);
-    s1.push(3);
-    s1.push(4);
     s1.push(5);
+    s1.push(7);
+    s1.push(9);
     s1.pop();
-    REQUIRE(s1.count() == 4);
-}
-
-TEST_CASE("op = ", "") {
-    stack<int> s1(0);
-    stack<int> s2 = s1;
-	REQUIRE(s2.count() == s1.count());
-	REQUIRE(s2.array_size() == s1.array_size());
+    REQUIRE(s1.count() == 2);
 }
